2aa2a9ea0e6a1ef1a6e02efb3809e48691d15a96
julian <juliancano.dev@gmail.com>
2020-06-13 15:40:04 -0500

    Taskes 0 1 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,7 +5,7 @@
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
-void swap_ints(int *a, int *b)
+void swap(int *a, int *b)
 {
 	int tmp;
 
@@ -16,7 +16,6 @@ void swap_ints(int *a, int *b)
 
 /**
  * selection_sort - Sort an array of integers in ascending order
- *                  using the selection sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
  *
@@ -28,17 +27,21 @@ void selection_sort(int *array, size_t size)
 	size_t i, j;
 
 	if (array == NULL || size < 2)
+    {
 		return;
+    }
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min = array + i;
 		for (j = i + 1; j < size; j++)
+        {
 			min = (array[j] < *min) ? (array + j) : min;
+        }
 
 		if ((array + i) != min)
 		{
-			swap_ints(array + i, min);
+			swap(array + i, min);
 			print_array(array, size);
 		}
 	}
